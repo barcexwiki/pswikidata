c3858efd2573f6648706a299516a81601c81f90c
barcex <barcexwiki@gmail.com>
2016-01-17 20:11:24 +0100

    Improving Add-WDStatement with more value types

--- PSWikidata/Cmdlets/AddWDStatement.cs
+++ PSWikidata/Cmdlets/AddWDStatement.cs
@@ -14,14 +14,37 @@ namespace PSWikidata
         ConfirmImpact = ConfirmImpact.Medium)]
     public class AddWDStatement : PSWDNetCmdlet
     {
-        [Parameter(Mandatory = true, ValueFromPipeline = true, HelpMessage = "Item to be modified.")]
+        [Parameter(Mandatory = true, ValueFromPipeline = true, HelpMessage = "Item to be modified.", ParameterSetName = "item")]
+        [Parameter(Mandatory = true, ValueFromPipeline = true, HelpMessage = "Item to be modified.", ParameterSetName = "monolingual")]
+        [Parameter(Mandatory = true, ValueFromPipeline = true, HelpMessage = "Item to be modified.", ParameterSetName = "string")]
         public PSWDItem Item { get; set; }
 
-        [Parameter(Mandatory = true, HelpMessage = "Property for the statement.")]
+        [Parameter(Mandatory = true, HelpMessage = "Property for the statement.", ParameterSetName = "item")]
+        [Parameter(Mandatory = true, HelpMessage = "Property for the statement.", ParameterSetName = "monolingual")]
+        [Parameter(Mandatory = true, HelpMessage = "Property for the statement.", ParameterSetName = "string")]
         public string Property { get; set; }
 
-        [Parameter(Mandatory = true, HelpMessage = "Item that will be the value of the property.")]
+        [Parameter(Mandatory = true, HelpMessage = "Item that will be the value of the property.", ParameterSetName = "item")]
         public string ValueItem { get; set; }
+
+        [Parameter(Mandatory = true, HelpMessage = "Text that will be the value of the property.", ParameterSetName = "monolingual")]
+        public string ValueText { get; set; }
+
+        [Parameter(Mandatory = true, HelpMessage = "Language code for the text that will be the value of the property.", ParameterSetName = "monolingual")]
+        public string ValueLanguage { get; set; }
+
+        [Parameter(Mandatory = true, HelpMessage = "Text that will be the value of the property.", ParameterSetName = "string")]
+        public string ValueString { get; set; }
+
+        [Parameter(Mandatory = false, HelpMessage = "Add the statement even if there is already an statement for this property.", ParameterSetName = "item")]
+        [Parameter(Mandatory = false, HelpMessage = "Add the statement even if there is already an statement for this property.", ParameterSetName = "monolingual")]
+        [Parameter(Mandatory = false, HelpMessage = "Add the statement even if there is already an statement for this property.", ParameterSetName = "string")]
+        public SwitchParameter Multiple
+        {
+            get { return multiple; }
+            set { multiple = value; }
+        }
+        private bool multiple;
 
 
         protected override void BeginProcessing()
@@ -29,22 +52,72 @@ namespace PSWikidata
             base.BeginProcessing();
         }
 
+        private bool IsDuplicatedStatement(Wikibase.DataValues.DataValue dataValue)
+        {
+            
+            Dictionary<string,Claim> claims = Item.ExtensionData.getClaimsForProperty(Property.ToUpper());
+
+            bool duplicate = false;
+
+            if (claims != null)
+            {
+                foreach (Claim c in claims.Values)
+                {
+                    if (c.mainSnak.DataValue.Equals(dataValue))
+                    {
+                        duplicate = true;
+                        break;
+                    }
+                }
+            }
+
+            return duplicate;
+        }
+
         protected override void ProcessRecord()
         {
 
-            Snak snak = new Snak(SnakType.Value, 
-                            new EntityId(Property),
-                            new Wikibase.DataValues.EntityIdValue(new EntityId(ValueItem))
-                            );
+            Wikibase.DataValues.DataValue dataValue;
 
-            Item.ExtensionData.createStatementForSnak(snak);
+            switch (this.ParameterSetName)
+            {
+                case "item":
+                    dataValue = new Wikibase.DataValues.EntityIdValue(new EntityId(ValueItem));
+                    break;
+                case "monolingual":
+                    dataValue = new Wikibase.DataValues.MonolingualTextValue(ValueText,ValueLanguage);
+                    break;
+                case "string":
+                    dataValue = new Wikibase.DataValues.StringValue(ValueString);
+                    break;
+                default:
+                    throw new Exception("Unidentified parameter set");
+            }
 
-            string comment = String.Format("Adding claim {0} {1}", Property, ValueItem);
-            Item.ExtensionData.save(comment);
 
-            Item.RefreshFromExtensionData();
+            if (Multiple || !IsDuplicatedStatement(dataValue))
+            {
 
-            WriteVerbose(comment);
+                if (ShouldProcess(Item.QId, "Adding claim"))
+                {
+
+                    Snak snak = new Snak(SnakType.Value,
+                                    new EntityId(Property),
+                                    dataValue
+                                    );
+
+                    Item.ExtensionData.createStatementForSnak(snak);
+
+                    string comment = String.Format("Adding claim {0} {1}", Property, dataValue.ToString());
+                    Item.ExtensionData.save(comment);
+
+                    Item.RefreshFromExtensionData();
+
+                    WriteVerbose(comment);
+
+                }
+            }
+
             WriteObject(Item, true);
 
         }
@@ -54,6 +127,7 @@ namespace PSWikidata
         {
             base.EndProcessing();
         }
+
 
     }
 
