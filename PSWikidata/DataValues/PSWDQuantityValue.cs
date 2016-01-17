a01603350aff93569dc464789b25d7accd358691
barcex <barcexwiki@gmail.com>
2016-01-17 20:12:00 +0100

    Treat the members of PSWDQuantityValue as decimal.

--- PSWikidata/DataValues/PSWDQuantityValue.cs
+++ PSWikidata/DataValues/PSWDQuantityValue.cs
@@ -4,27 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace PSWikidata.DataValues
 {
     class PSWDQuantityValue
     {
-        public string Amount { get; set; }
-        public string LowerBound { get; set; }
-        public string UpperBound { get; set; }
+        public decimal Amount { get; set; }
+        public decimal LowerBound { get; set; }
+        public decimal UpperBound { get; set; }
         public string Unit { get; set; }
 
         internal PSWDQuantityValue(Wikibase.DataValues.QuantityValue q)
         {
-            Amount = q.Amount;
-            LowerBound = q.LowerBound;
-            UpperBound = q.UpperBound;
+            Amount = decimal.Parse(q.Amount,CultureInfo.InvariantCulture);
+            LowerBound = decimal.Parse(q.LowerBound,CultureInfo.InvariantCulture);
+            UpperBound = decimal.Parse(q.UpperBound, CultureInfo.InvariantCulture);
             Unit = q.Unit;
         }
 
         public override string ToString()
         {
-            return Amount + " / " + LowerBound + " / " + UpperBound + " / " + Unit;
+            string unitSuffix = (Unit == "1") ? "" : " / " + Unit;
+
+
+
+            decimal minus = Math.Abs(Amount - LowerBound);
+            decimal plus = Math.Abs(Amount - UpperBound);
+
+            if (minus == plus && plus == 0)
+            {
+                return Amount + unitSuffix;
+            }
+            if (minus == plus)
+            {
+                return Amount + "±" + plus + unitSuffix;
+            }
+            return Amount + " +" + plus + " -" + minus + unitSuffix;
         }
     }
 }
