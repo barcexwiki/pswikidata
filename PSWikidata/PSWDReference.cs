e62198c05b39d385b87aaf7dae3aa59a94527de6
barcex <barcexwiki@gmail.com>
2016-02-12 22:29:36 +0100

    reference class

---
+++ PSWikidata/PSWDReference.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PSWikidata.DataValues;
+using Wikibase;
+using Wikibase.DataValues;
+
+namespace PSWikidata
+{
+    public class PSWDReference
+    {
+
+        public PSWDSnak[] Snaks
+        {
+            get { return _snaks.ToArray(); }
+        }
+
+        private List<PSWDSnak> _snaks = new List<PSWDSnak>();
+
+        internal PSWDReference(Reference reference)
+        {
+            foreach (Snak snak in reference.Snaks)
+            {
+                _snaks.Add(new PSWDSnak(snak));
+            }
+        }
+    }
+}
