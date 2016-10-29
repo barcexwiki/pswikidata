e3c3383f6741a0520362c240e9568f01dbf7e493
barcexwiki <barcexwiki@gmail.com>
2016-10-29 21:26:32 +0200

    Get-WDSitelinkTitle cmdlet

---
+++ PSWikidata/Cmdlets/GetWDSitelinkTitle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using Wikibase;
+
+namespace PSWikidata
+{
+    [Cmdlet(VerbsCommon.Get, "WDSitelinkTitle",
+        SupportsShouldProcess = true,
+        ConfirmImpact = ConfirmImpact.None)]
+    public class GetWDSitelinkTitle: PSCmdlet
+    {
+        [Parameter(
+           Mandatory = true,
+           ValueFromPipeline = true,
+           Position = 1,
+           HelpMessage = "Item to be read."
+        )]
+        [PSWDItemArgumentTransformation]
+        public PSWDItem Item { get; set; }
+
+        [Parameter(
+           Mandatory = true,
+           Position = 2,
+           HelpMessage = "Site to be used."
+        )]
+        public string Site { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            string title = Item.ExtensionData.GetSitelink(Site);
+            if (title != null) WriteObject(title);
+        }
+    }
+}
