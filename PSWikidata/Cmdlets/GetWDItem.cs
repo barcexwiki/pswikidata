7b8dd0b8abc3c5e1cb6baf8f98b86b06101705b7
barcexwiki <barcexwiki@gmail.com>
2016-11-07 19:48:39 +0100

    Get-WDItem: adding a parameter set to get the iten given a sitelink

--- PSWikidata/Cmdlets/GetWDItem.cs
+++ PSWikidata/Cmdlets/GetWDItem.cs
@@ -17,13 +17,20 @@ namespace PSWikidata
            ValueFromPipelineByPropertyName = true,
            ValueFromPipeline = true,
            Position = 0,
-           HelpMessage = "Q identifier for the item."
+           HelpMessage = "Q identifier for the item.",
+           ParameterSetName = "qid"
         )]
         public string[] QId
         {
             get { return _qIdCollection; }
             set { _qIdCollection = value; }
         }
+
+        [Parameter(Mandatory = true, HelpMessage = "Site of the sitelink", ParameterSetName = "sitelink")]
+        public string SitelinkSite { get; set; }
+
+        [Parameter(Mandatory = true, HelpMessage = "Title on the specified SitelinkSite", ParameterSetName = "sitelink")]
+        public string SitelinkTitle { get; set; }
 
         protected override void BeginProcessing()
         {
@@ -32,15 +39,32 @@ namespace PSWikidata
 
         protected override void ProcessRecord()
         {
-            foreach (string q in _qIdCollection)
+            Item item;
+
+            switch (ParameterSetName)
             {
-                WriteVerbose("Getting item " + q);
-                Item item = (Item)provider.GetEntityFromId(new EntityId(q));
-                if (item != null)
-                {
-                    WriteObject(new PSWDItem(item));
-                }
+                case "qid":
+                    foreach (string q in _qIdCollection)
+                    {
+                        WriteVerbose("Getting item " + q);
+                        item = (Item)provider.GetEntityFromId(new EntityId(q));
+                        if (item != null)
+                        {
+                            WriteObject(new PSWDItem(item));
+                        }
+                    }
+                    break;
+                case "sitelink":
+                    item = (Item)provider.GetEntityFromSitelink(SitelinkSite, SitelinkTitle);
+                    if (item != null)
+                    {
+                        WriteObject(new PSWDItem(item));
+                    }
+                    break;
+                default:
+                    throw new Exception("Unidentified parameter set");
             }
+
         }
 
         protected override void EndProcessing()
