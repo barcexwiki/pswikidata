66cf7c6cca1407575a58be075b31f1f580f086c8
barcexwiki <barcexwiki@gmail.com>
2017-05-06 21:50:08 +0200

    PSWDTimeValue.cs adding properties for Day/Month/Year/Hour/Minute/Second

--- PSWikidata/DataValues/PSWDTimeValue.cs
+++ PSWikidata/DataValues/PSWDTimeValue.cs
@@ -13,6 +13,43 @@ namespace PSWikidata.DataValues
         public string Time { get; set; }
 
 
+        public long Year
+        {
+            get;
+            set;
+        }
+
+        public int Month
+        {
+            get;
+            set;
+        }
+
+        public int Day
+        {
+            get;
+            set;
+        }
+
+        public int Hour
+        {
+            get;
+            set;
+        }
+
+        public int Minute
+        {
+            get;
+            set;
+        }
+
+        public int Second
+        {
+            get;
+            set;
+        }
+
+
         public Int32 Before
         {
             get;
@@ -51,6 +88,12 @@ namespace PSWikidata.DataValues
             Precision = t.Precision;
             TimeZoneOffset = t.TimeZoneOffset;
             DisplayCalendarModel = t.DisplayCalendarModel;
+            Year = t.Year;
+            Month = t.Month;
+            Day = t.Day;
+            Hour = t.Hour;
+            Hour = t.Minute;
+            Hour = t.Second;
         }
 
         public override string ToString()
